c87694104a2e58321aa18b835fde95919c244f3b
Matteo-Michels <matteo.michels@leerling.kov.be>
2023-01-24 13:54:07 +0100

    vragen.py aangemaakt
    Heb vragen.py aangemaakt en een lege fullscreen applicatie aangemaakt.

---
+++ vragen.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+from tkinter import *
+from tkinter import ttk
+import os
+from PIL import *
+
+root = Tk()
+root.attributes('-fullscreen', True)
+
+bg = PhotoImage(file = "bg_vraag.png")
+
+imagelabel = Label(root, image = bg)
+imagelabel.place(x = 0, y = 0,relwidth = 1, relheight = 1)
+
+root.mainloop()
