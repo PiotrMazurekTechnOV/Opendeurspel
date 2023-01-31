529f8b434b0d142780076eee669e668e4fa32f79
kaanincirli <kaan.incirli@leerling.kov.be>
2023-01-31 13:53:06 +0100

    opvraag van klas systeem klaar maken voor DB

--- Questions/vraag_preperation.py
+++ Questions/vraag_preperation.py
@@ -3,34 +3,58 @@ import vraag2_multipleChoise
 import vraag3_choose
 from tkinter import *
 
-vraagKeuze = 1
+vraagKeuze = 4
 
 main = Tk()
 
-width = 600
-height = 600
+if vraagKeuze == 4:
 
-def submit():  # Callback function for SUBMIT Button
-    klas = textbox.get("1.0", END)  # For line 1, col 0 to end.
-    print(klas)
+    width = 600
+    height = 600
 
-c = Canvas(main, width=width, height=height, highlightthickness=0)
-c.pack()
+    def submit():  # Callback function for SUBMIT Button
+        klas = textbox.get("1.0", END)  # For line 1, col 0 to end.
+        print(klas)
 
-textbox = Text(c, width=30, height=1)
-textbox.pack()
 
-submitbutton = Button(c, width=10, height=1, text='SUBMIT', command=submit)
-submitbutton.pack()
+        #database compare klas met ID en vraag de soort
+        vraagKeuze = 0
 
-mainloop()
 
-if vraagKeuze == 0:
+        if vraagKeuze == 0:
+            print("0")
+            main.destroy()
+            vraag1_openVraag.SetUp()
+        elif vraagKeuze == 1:
+            print("1")
+            main.destroy()
+            vraag2_multipleChoise.SetUp()
+        elif vraagKeuze == 2:
+            print("2")
+            main.destroy()
+            vraag3_choose.SetUp()
+            
+    c = Canvas(main, width=width, height=height, highlightthickness=0)
+    c.pack()
+
+    textbox = Text(c, width=30, height=1)
+    textbox.pack()
+
+    submitbutton = Button(c, width=10, height=1, text='SUBMIT', command=submit)
+    submitbutton.pack()
+
+    mainloop()
+elif vraagKeuze == 0:
     print("0")
     vraag1_openVraag.SetUp()
+    main.destroy()
 elif vraagKeuze == 1:
     print("1")
     vraag2_multipleChoise.SetUp()
-else:
+    main.destroy()
+elif vraagKeuze == 2:
     print("2")
     vraag3_choose.SetUp()
+    main.destroy()
+else:
+    print("err")
