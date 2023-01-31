63e3a59f4d49e5f811a9c4fb14e055fcdee047a3
Dugske <rayan.dadsi@leerling.kov.be>
2023-01-31 11:10:04 +0100

    added the buttons

--- admin_panel.py
+++ admin_panel.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import ttk
 from PIL import *
 from PIL import ImageTk, Image
     
@@ -9,25 +10,48 @@ class Pagina(Frame):
     def show(self):
         self.lift()
 
-class HomePagina(Pagina):
+class VragenPagina(Pagina):
    def __init__(self, *args, **kwargs):
         Pagina.__init__(self, *args, **kwargs)
 
-        #titel
-        welkom = Label(self, text = "Welkom op de opendeurdag van Technov!"
-               , fg="#1b709d", font=("Gilroy Light", 65))
-        welkom.place(relx=0.5, rely=0.1, anchor=N)
+        geselecteerde_vraag = StringVar(self)
+        vragen = ["vraag1", "vraag2"]
 
-        #uitleg
-        uitleg = Label(self, text = """Dit is het opendeurspel gemaakt door de leerlingen Industriele ICT.
-Tijdens u traject op onze school zal u vragen kunnen invullen met uw ID.
-Als u hieronder op Start! duwt.
-Gaat u naar de registratiepagina waar u uw persoonlijk quiz ID ontvangt!
-Op het einde van je rondleiding zal u uw score kunnen afdrukken als een 'diploma'!
-Veel plezier!""", fg="#1b709d", font=("gilroy light",35), pady=50)
-        uitleg.place(relx=0.5, rely=0.25, anchor=N)
+        #selecteer vraag menu
+        selecteer_vraag = OptionMenu(self, geselecteerde_vraag, *vragen)
+        selecteer_vraag.configure(bd = 1, indicatoron=0, height=2, width=10)
+        selecteer_vraag.place(relx=0.5, rely=0.2, anchor=N)
 
-class GegevensPagina(Pagina):
+        #vraag aanmaak knop
+        maak_vraag = Button(self, text="Creer nieuwe vraag")
+        maak_vraag.place(relx=0.5, rely=0.3, anchor=N)
+
+        #vraag naam text
+        vraag_naam = Entry(self)
+        vraag_naam.place(relx=0.5, rely=0.35, anchor=N)
+
+        #vraag delete knop
+        delete_vraag = Button(self, text="Verwijder vraag")
+        delete_vraag.place(relx=0.5, rely=0.4, anchor=N)
+
+        #weizig antwoorden vraag knop
+        antwoorden_aanpassen = Button(self, text="Antwoorden aanpassen")
+        antwoorden_aanpassen.place(relx=0.5, rely=0.45, anchor=N)
+
+        #toon alle antwoorden treeview
+        antwoorden = ttk.Treeview(self)
+        antwoorden.place(relx=0.5, rely=0.5, anchor=N)
+
+        def itemClicked(self):
+            print("a", antwoorden.focus())
+
+        antwoorden.insert('', 'end', text="Antwoord 1", tags=('ttk',))
+        antwoorden.tag_bind('ttk', '<1>', itemClicked) 
+        
+
+
+
+class AntwoordenPagina(Pagina):
    def __init__(self, *args, **kwargs):
         Pagina.__init__(self, *args, **kwargs)
         #canvas = Canvas(self, width="100c", height="100c", bg="#1b709d")
@@ -45,8 +69,8 @@ class GegevensPagina(Pagina):
 class MainView(Frame):
     def __init__(self, *args, **kwargs):
         Frame.__init__(self, *args, **kwargs)
-        p1 = HomePagina(self)
-        p2 = GegevensPagina(self)
+        p1 = VragenPagina(self)
+        p2 = AntwoordenPagina(self)
 
         buttonframe = Frame(self)
         container = Frame(self)
@@ -57,10 +81,15 @@ class MainView(Frame):
         p2.place(in_=container, x=0, y=0, relwidth=1, relheight=1)        
 
         #b1 = Button(p2, text="Klaar!", bg="#D5DF3A", fg="#FFFFFF", font=("Gilroy", 20), relief= FLAT , command=p1.lift, activeforeground="#FFFFFF", activebackground="#1b709d")
-        b2 = Button(p1, text=" Start! ", bg="#D5DF3A", fg="#FFFFFF", font=("Gilroy", 30), relief= FLAT , command=p2.lift, activeforeground="#FFFFFF", activebackground="#1b709d")        
+        #b2 = Button(p1, text=" Start! ", bg="#D5DF3A", fg="#FFFFFF", font=("Gilroy", 30), relief= FLAT , command=p2.lift, activeforeground="#FFFFFF", activebackground="#1b709d")        
 
         #b1.place(relx=.5 ,rely=0.875, relwidth=.1, anchor=CENTER) 
-        b2.place(relx=.5 ,rely=0.875, relwidth=.15, anchor=CENTER) 
+        #b2.place(relx=.5 ,rely=0.875, relwidth=.15, anchor=CENTER) 
+
+        #titel
+        welkom = Label(self, text = "Admin panel"
+               , fg="#1b709d", font=("Gilroy Light", 30))
+        welkom.place(relx=0.5, rely=0.05, anchor=N)
 
         p1.show()
 
@@ -69,6 +98,10 @@ if __name__ == "__main__":
     main = MainView(root)
     main.pack(side="top", fill="both", expand=True)
     root.wm_geometry("400x400")
-    #root.iconbitmap('kovlogo.ico')
+    root.state('zoomed')
+    root.title("Admin panel")
+    root.iconbitmap('kovlogo.ico')
     #root.attributes('-fullscreen', True)
     root.mainloop()
+
+#Vragen toevoegen/ aanpassen, antwoorden aanpassen
