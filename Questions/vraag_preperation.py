04491eec76a07a435eb2bdb772e75841e5fa2069
kaanincirli <kaan.incirli@leerling.kov.be>
2023-02-09 10:48:06 +0100

    vraag 2 debug

--- Questions/vraag_preperation.py
+++ Questions/vraag_preperation.py
@@ -4,64 +4,55 @@ import vraag3_choose
 from tkinter import *
 import database as db
 
-vraagKeuze = 4
+
 main = Tk()
 
-if vraagKeuze == 4:
+width = 600
+height = 600
 
-    width = 600
-    height = 600
+def submit():  # Callback function for SUBMIT Button
+    klas = textbox.get("1.0", "end-1c")  # For line 1, col 0 to end.
+    print(klas)
+    idk = db.select_question(klas)
+    idkk = db.select_answer(klas)
 
-    def submit():  # Callback function for SUBMIT Button
-        klas = textbox.get("1.0", "end-1c")  # For line 1, col 0 to end.
-        print(klas)
-        idk = db.select_question(klas)
-        idkk = db.select_answer(klas)
+    type = idk[2]
+    vraag = idk[1]
+    antwoorden = idkk[1]
+    resultaten = idkk[3]
 
-        type = idk[2]
-        vraag = idk[1]
-        antwoorden = idkk[1]
-        resultaten = idkk[3]
-        print(resultaten)
+    #database compare klas met ID en vraag de soort
+    vraag = str(vraag)
+    print(vraag)
+    vraagKeuze = int(type)
+    print(vraagKeuze)
+    antwoorden = str(antwoorden)
+    print(antwoorden)
+    resultaten = str(resultaten)
+    print(resultaten)
+    print("--------")
 
-        #database compare klas met ID en vraag de soort
-        vraagKeuze = int(type)
 
-
-        if vraagKeuze == 0:
+    if vraagKeuze == 0:
             print("0")
             main.destroy()
             vraag1_openVraag.SetUp()
-        elif vraagKeuze == 1:
+    elif vraagKeuze == 1:
             print("1")
             main.destroy()
             vraag2_multipleChoise.SetUp(vraag,resultaten,antwoorden)
-        elif vraagKeuze == 2:
+    elif vraagKeuze == 2:
             print("2")
             main.destroy()
             vraag3_choose.SetUp()
             
-    c = Canvas(main, width=width, height=height, highlightthickness=0)
-    c.pack()
+c = Canvas(main, width=width, height=height, highlightthickness=0)
+c.pack()
 
-    textbox = Text(c, width=30, height=1)
-    textbox.pack()
+textbox = Text(c, width=30, height=1)
+textbox.pack()
 
-    submitbutton = Button(c, width=10, height=1, text='SUBMIT', command=submit)
-    submitbutton.pack()
+submitbutton = Button(c, width=10, height=1, text='SUBMIT', command=submit)
+submitbutton.pack()
 
-    mainloop()
-elif vraagKeuze == 0:
-    print("0")
-    vraag1_openVraag.SetUp()
-    main.destroy()
-elif vraagKeuze == 1:
-    print("1")
-    vraag2_multipleChoise.SetUp()
-    main.destroy()
-elif vraagKeuze == 2:
-    print("2")
-    vraag3_choose.SetUp()
-    main.destroy()
-else:
-    print("err")
+mainloop()
