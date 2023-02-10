57cc87c17af230c3504a84c21e42be8cda501fde
Daan-Caron <daan.caron@leerling.kov.Be>
2023-02-10 11:24:10 +0100

    meerkeuzenvraag klaar
    ik ga nu keyboard aan het werken te doen krijgen

--- vraag.py
+++ vraag.py
@@ -6,9 +6,8 @@ import re
 
 class question():
     location = "ICT lokaal"
-    question_id = "1"
     question = "In welke taal is dit geschreven"
-    answers = ["Python", "C#","HTML"]
+    answers = ["Python", "C#", "Java"]
     
     
     
@@ -23,23 +22,22 @@ class Pagina(Frame):
 class Vraag(Pagina):
     def __init__(self, *args, **kwargs):
         Pagina.__init__(self, *args, **kwargs)
-        rely_v = 0.4
         qu = question()
           
         #titel
         location_label = Label(self, text = qu.location, fg="#1b709d", font=("Gilroy Light", 65))
         location_label.place(relx=0.5, rely=0.1, anchor=N)
         
-        for x in range(0, len(qu.answers)):
-            ans_name = "c" + str(x)
-            ans_var = "a" + str(x)
-            ans_var = IntVar()
-            ans_name = Button(self, text=str(qu.answers[x]), bg="#D5DF3A", fg="#FFFFFF", activeforeground="#FFFFFF", activebackground="#1b709d", font=("gilroy light",25), pady=50, width=10, height=1)
-            ans_name.place(relx=0.5, rely=rely_v, anchor=N)
-            rely_v = rely_v + 0.2
-        
-        vraag_label = Label(self, text ="Vraag: "+ qu.question_id +")  "+qu.question, fg="#1b709d", font=("gilroy light",35), pady=50)
+        vraag_label = Label(self, text ="Vraag: "+qu.question, fg="#1b709d", font=("gilroy light",35), pady=50)
         vraag_label.place(relx=0.5, rely=0.25, anchor=N)
+        
+        b1 = Button(self, text="Python", bg="#D5DF3A", fg="#FFFFFF", activeforeground="#FFFFFF", activebackground="#1b709d", font=("gilroy light",25), pady=50, width=10, height=1)
+        b2 = Button(self, text="C#", bg="#D5DF3A", fg="#FFFFFF", activeforeground="#FFFFFF", activebackground="#1b709d", font=("gilroy light",25), pady=50, width=10, height=1)
+        b3 = Button(self, text="Java", bg="#D5DF3A", fg="#FFFFFF", activeforeground="#FFFFFF", activebackground="#1b709d", font=("gilroy light",25), pady=50, width=10, height=1)
+        
+        b1.place(relx=0.5, rely=0.4, anchor=N)
+        b2.place(relx=0.5, rely=0.6, anchor=N)
+        b3.place(relx=0.5, rely=0.8, anchor=N)
         
         
 
@@ -53,17 +51,18 @@ class MainView(Frame):
         
         canvas = Canvas(container, width=1920, height=1080)
         canvas.pack()
-        
-        #----------------TEXT EN FOTOS OP HETZELFDE MOMENT LUKT NIET DAAROM STAAT ALLES IN COMMENTAAR ------------
-        
-        #background = PhotoImage(file="bg_vraag.png")
-        #canvas.create_image(960,540,image=background)
 
-        #logo = PhotoImage(file="technov-logo.png")
-        #canvas.create_image(300,125,image=logo)
+        logo = PhotoImage(file="technov-logo.png")
+        label1 = Label(image=logo)
+        label1.image = logo
+        label1.place(x=1, y=1)
         
-        #---------------------------------------------------------------------------------------------------------
+        kalspng = PhotoImage(file="Klas.png")
+        label1 = Label(image=kalspng)
+        label1.image = kalspng
+        label1.place(relx=0.81, rely=0.85)
         
+              
         p1.place(in_=container, x=0, y=0, relwidth=1, relheight=1)    
         p1.show()
   
@@ -76,5 +75,4 @@ if __name__ == "__main__":
     root.iconbitmap('kovlogo.ico')
     root.title('opendeurdag')
     main.pack(side="top", fill="both", expand=True)
-    #verander de foto onder de naam bg_vraag.png in file explorer om de achtergrond aan te passen
     root.mainloop()
