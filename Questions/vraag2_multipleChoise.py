4adcf682a6cf695debb812b1d88034de5036c55c
kaanincirli <kaan.incirli@leerling.kov.be>
2023-02-13 13:20:59 +0100

    vraag 2 klaar

--- Questions/vraag2_multipleChoise.py
+++ Questions/vraag2_multipleChoise.py
@@ -9,13 +9,17 @@ import re
 # option_c_check = datbase
 # option_d_check = datbase
 
-def SetUp(vraag, resultaten, antwoorden):    
+def SetUp(vraag, resultaten, antwoorden):  
+    global resultatenn
+    resultatenn = resultaten
     class Quiz(tk.Tk):
         def __init__(self):
             super().__init__()
-
+            global resultatenn
             if re.search(",",antwoorden) != None:
                 self.answers = antwoorden.split(",")
+
+            global correct_answer
 
             self.title("Quiz")
             self.question = tk.StringVar()
@@ -33,10 +37,43 @@ def SetUp(vraag, resultaten, antwoorden):
                 print("No Qeustion 4")
                 
             
+            if re.search(",",resultatenn) != None:
+                resultatenn.replace("1","a")
+                resultatenn.replace("2","b")
+                resultatenn.replace("3","c")
+                resultatenn.replace("4","d")
+                if re.search("a",resultatenn) == None:
+                    resultatenn = resultatenn + ",na"
+                if re.search("b",resultatenn) == None:
+                    resultatenn = resultatenn + ",nb"
+                if re.search("c",resultatenn) == None:
+                    resultatenn = resultatenn + ",nc"
+                if re.search("d",resultatenn) == None:
+                    resultatenn = resultatenn + ",nd"
+                self.correct_answers = resultatenn.split(",")
+                self.correct_answers += None
+                correct_answer = self.correct_answers
+                print(correct_answer)
+            else:
+                if resultatenn == "1":
+                    self.correct_answers = {"a","nb","nc","nd",None}
+                    correct_answer = self.correct_answers
+                    print(correct_answer)
+                if resultatenn == "2":
+                    self.correct_answers = {"na","b","nc","nd",None}
+                    correct_answer = self.correct_answers
+                    print(correct_answer)
+                if resultatenn == "3":
+                    self.correct_answers = {"na","nb","c","nd",None}
+                    correct_answer = self.correct_answers
+                    print(correct_answer)
+                if resultatenn == "4":
+                    self.correct_answers = {"na","nb","nc","d",None}
+                    correct_answer = self.correct_answers
+                    print(correct_answer)
             
-            if re.search(",",resultaten) != None:
-                self.correct_answers = resultaten.split(",")
             #self.correct_answers = {"a", "b","c","nd",None}  -- oude code
+            correct_answer = self.correct_answers
             self.create_widgets()
 
         def create_widgets(self):
@@ -75,11 +112,17 @@ def SetUp(vraag, resultaten, antwoorden):
                 print(varD.get())
                 ######################################################
             }
-            if selected_answers == self.correct_answers:
+            if selected_answers == correct_answer:
                 result_label = tk.Label(self, text="Correct!")
                 print("")
                 print(selected_answers)
-                print(self.correct_answers)
+                print("---")
+            else:
+                print("not corect!")
+                print(correct_answer)
+                print("---")
+                result_label = tk.Label(self, text="Not Correct!")
+                print(selected_answers)
                 print("---")
             selected_answers.clear()
             result_label.pack()
