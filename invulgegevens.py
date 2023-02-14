312e654746b3a45b4e4af65cf21d67f36d38da78
Dugske <rayan.dadsi@leerling.kov.be>
2023-02-14 14:09:39 +0100

    added always active keyboard and final registration page

--- invulgegevens.py
+++ invulgegevens.py
@@ -189,164 +189,49 @@ class GegevensPagina(Pagina):
         return False
    
 
-        
+class SuccesPage(Pagina):
+    def __init__(self, *args, **kwargs):
+        Pagina.__init__(self, *args, **kwargs)
+        welkom = Label(self, text = "Welkom op de opendeurdag van Technov!", fg="#1b709d", font=("Gilroy Light", 36))
+        welkom.place(relx=0.5, rely=0.05, anchor=N)
+
+        #uitleg
+        self.uitleg = Label(self, text = "Uw ID is: ", fg="#1b709d", font=("gilroy light", 24))
+        self.uitleg.place(relx=0.5, rely=0.15, anchor=N)
+    
+    def show_with_id(self, id):
+        self.uitleg.config(text="Uw ID is: " + id)
+        self.lift()
 
 class MainView(Frame):
     def __init__(self, *args, **kwargs):
         Frame.__init__(self, *args, **kwargs)
         p1 = HomePagina(self)
         p2 = GegevensPagina(self)
+        p3 = SuccesPage(self)
 
-        buttonframe = Frame(self)
         container = Frame(self)
-        buttonframe.pack(side="top", fill="x", expand=False)
         container.pack(side="top", fill="both", expand=True)
 
         p1.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
-        p2.place(in_=container, x=0, y=0, relwidth=1, relheight=1)        
+        p2.place(in_=container, x=0, y=0, relwidth=1, relheight=1)       
+        p3.place(in_=container, x=0, y=0, relwidth=1, relheight=1)    
 
         def klaar():
-            if (p2.gegevens_ingevuld()):
-                p1.lift()
-            
+            result = p2.gegevens_ingevuld()
+            if (result and result[0]):
+                p3.show_with_id(result[1])
+            print(result[1])
 
-        #b1 = Button(p2, text="Klaar!", bg="#D5DF3A", fg="#FFFFFF", font=("Gilroy", 20), relief= FLAT , command=p1.lift, activeforeground="#FFFFFF", activebackground="#1b709d")
-        b2 = Button(p1, text=" Start! ", bg="#D5DF3A", fg="#FFFFFF", font=("Gilroy", 30), relief= FLAT , command=p2.lift, activeforeground="#FFFFFF", activebackground="#1b709d")        
-        b1 = Button(p2, text=" Klaar! ", bg="#D5DF3A", fg="#FFFFFF", font=("Gilroy", 20), relief= FLAT , command=klaar, activeforeground="#FFFFFF", activebackground="#1b709d")        
+        b1 = Button(p1, text=" Start! ", bg="#D5DF3A", fg="#FFFFFF", font=("Gilroy", 30), relief= FLAT , command=p2.lift, activeforeground="#FFFFFF", activebackground="#1b709d")        
+        b2 = Button(p2, text=" Klaar! ", bg="#D5DF3A", fg="#FFFFFF", font=("Gilroy", 20), relief= FLAT , command=klaar, activeforeground="#FFFFFF", activebackground="#1b709d")  
+        b3 = Button(p3, text=" Klaar! ", bg="#D5DF3A", fg="#FFFFFF", font=("Gilroy", 20), relief= FLAT , command=p1.lift, activeforeground="#FFFFFF", activebackground="#1b709d")              
 
-        #b1.place(relx=.5 ,rely=0.875, relwidth=.1, anchor=CENTER) 
-        b2.place(relx=.5 ,rely=0.875, relwidth=.15, anchor=CENTER) 
-        b1.place(relx=.5 ,rely=0.875, relwidth=.1, anchor=CENTER) 
+        b1.place(relx=.5 ,rely=0.875, relwidth=.15, anchor=CENTER) 
+        b2.place(relx=.5 ,rely=0.875, relwidth=.1, anchor=CENTER) 
+        b3.place(relx=.5 ,rely=0.875, relwidth=.1, anchor=CENTER) 
 
         p1.show()
-
-def new_window():
-    #klaarbutton
-    def klbutton():
-        global adressin, numm, eoudin
-        naamin = gegevens_naam.get()
-        adressin = gegevens_adress.get()
-        achin = gegevens_acnaam.get()
-        leefin = gegevens_leef.get()
-        studin = ""
-        for j in studiebox.curselection():
-            studin = better_string(studiebox.get(j))
-        noudin = gegevens_oudnaam.get()
-        eoudin = gegevens_oudadress.get()
-        telein = gegevens_tel.get()
-
-        ##### check e-mail
-
-        if(re.fullmatch(regex, adressin))and(re.fullmatch(regex, eoudin)) and leefin.isnumeric() and telein.isnumeric():
-
-            print("Valid Email")
-            #Mazurek: user input valideren voordat het in de query wordt geplaatst
-            add =("INSERT INTO users(naam, email , achternaam , leeftijd, richting , naam_ouder , email_ouder , telefoonnummer) VALUES(%s,%s,%s,%s,%s,%s,%s,%s)")
-            val = (naamin, adressin, achin, leefin, studin, noudin, eoudin, telein)
-            #my_conn.execute(add,val)
-            #my_connect.commit()
-
-            #volgendebutton.place(relx=0.5, rely=0.7, anchor=CENTER)
-
-            klaarbutton.place_forget()
-            emailfout.place_forget()
-
-            volgendebutton.place_forget()
-            nummer.configure(text="Wil je door ons gecontacteert worden?",font=("Constantia",30))
-            cntctbox.place(relx=0.5, rely=0.75, anchor=CENTER)
-
-            nummer.place(relx=0.5, rely=0.7, anchor=CENTER)
-
-            adressin = gegevens_adress.get()
-            #my_conn.execute(("SELECT ID FROM users WHERE email = %s"),(adressin,))
-            #num = my_conn.fetchone()
-            #my_connect.commit()
-            numm = str(num).replace(',','').replace('(','').replace(')','').replace("'",'')
-        
-            volgendebutton.place(relx=0.5, rely=0.9, anchor=CENTER)
-            print(numm)
-
-        else:
-
-            print("Invalid Email")
-            emailfout.place(relx=0.5, rely=0.9, anchor=CENTER)
-            
-        ##### 
-    #contactbutton
-    def vlgndbutton():
-        global cont, numm, adressin
-        #cont = ""
-        print(numm)
-        #mag je gecontacteert worden?
-        for i in cntctbox.curselection():
-            if cntctbox.get(i) == "Ja":
-                #my_conn.execute(("UPDATE users SET contact = 1 WHERE ID = %s"),(numm,))
-                print (numm)
-                #my_connect.commit()
-            if cntctbox.get(i) == "Nee":
-                pass
-                #my_conn.execute(("UPDATE users SET contact = 0 WHERE ID = %s"),(numm,))
-                #my_connect.commit()
-        cntctbox.selection_clear(0,END)
-        cntctbox.place_forget()
-        volgendebutton.place_forget()
-        nummer.configure(text = "Uw nummer is: " + numm + "\n Vergeet dit nummer niet!"
-                            ,font = ("Constantia",20))
-        nummer.place(relx=0.5, rely=0.7, anchor=CENTER)
-        okbutton.place(relx=0.5, rely=0.8, anchor=CENTER)
-        '''for i in cntctbox.curselection():
-            if cntctbox.get(i) == "Ja":
-                okbutton.place(relx=0.5, rely=0.8, anchor=CENTER)
-                cont = cntctbox.get(i)
-            if cntctbox.get(i) == "Nee":
-                okbutton.place(relx=0.5, rely=0.8, anchor=CENTER)
-                cont = cntctbox.get(i)    '''      
-    
-    #okbutton
-    def okbutton():
-        #okbutton.place_forget()
-        global numm
-        nummer.configure(text = "Uw nummer is: " + numm + "\n Vergeet dit nummer niet!"
-                            ,font = ("Constantia",20))
-        nummer.place(relx=0.5, rely=0.7, anchor=CENTER)
-            
-            #delete dingen in entry
-        gegevens_naam.delete(0,END)
-        gegevens_adress.delete(0,END)
-        gegevens_acnaam.delete(0,END)
-        gegevens_leef.delete(0,END)
-        gegevens_oudnaam.delete(0,END)
-        gegevens_oudadress.delete(0,END)
-        gegevens_tel.delete(0,END)
-        studiebox.selection_clear(0,END)
-        klaarbutton.place(relx=0.5, rely=0.8, anchor=CENTER)
-        #finbutton.place(relx=0.5, rely=0.7, anchor=CENTER)
-        okbutton.place_forget()
-        nummer.place_forget()
-
-    def finbutton():
-        #klaarbuton.place_forget()
-        klaarbuton.place_forget()
-        finbutton.place_forget()
-        nummer.place_forget()
-        klaarbutton.place(relx=0.5, rely=0.7, anchor=CENTER)
-
-    klaarbutton = Button(root, text='klaar', bg="#502E93", fg="#F9DEE3", font=("Constantia", 15), relief = GROOVE , command=klbutton)
-    klaarbutton.place(relx=0.5, rely=0.8, anchor=CENTER)
-    volgendebutton = Button(root, text='volgende', bg="#502E93", fg="#F9DEE3", font=("Constantia",15),relief = GROOVE, command=vlgndbutton)
-    emailfout = Label(root, text = "foute e-mail", bg="#DF3740", font=("Constantia",15),relief = GROOVE)
-
-    okbutton = Button(root, text='Ok', bg="#502E93", fg="#F9DEE3", font=("Constantia",15),relief = GROOVE, command=okbutton)
-    finbutton = Button(root, text='Fin', bg="#502E93", fg="#F9DEE3", font=("Constantia",15),relief = GROOVE, command=finbutton)
-    cntctbox = Listbox(root,listvariable = StringVar(value =("Ja","Nee")), height = 2, exportselection=False, bg="#502E93", fg="#F9DEE3", font=("Constantia",10),relief = GROOVE)
-    cntctbox.bind('<<ListBoxSelect>>', okbutton)
-    nummer = Label(root)
-
-def welcome_screen():
-    #create button that will be placed
-    button = Button(root, text="new window", bg='black', fg='#469A00',
-                              command=lambda: new_window())
-    button.place(relx=0.5, rely=0.8, anchor=CENTER)
 
 if __name__ == "__main__":
     root = Tk()
---
+++ keyboard_always_active.py
@@ -0,0 +1,301 @@
+'''Popup Keyboard is a module to be used with Python's Tkinter library
+It subclasses the Entry widget as KeyboardEntry to make a pop-up
+keyboard appear when the widget gains focus. Still early in development.
+'''
+
+from tkinter import *
+
+
+class _PopupKeyboard(Toplevel):
+    '''A Toplevel instance that displays a keyboard that is attached to
+    another widget. Only the Entry widget has a subclass in this version.
+    '''
+    
+    def __init__(self, parent, attach, x, y, keycolor, textcolor, keysize=2, keyheight=1, font=('Arial', 20)):
+        Toplevel.__init__(self, takefocus=0)
+        
+        self.overrideredirect(True)
+        self.attributes('-alpha',0.95)
+
+        self.parent = parent
+        self.attach = attach
+        self.keysize = keysize
+        self.keyheight = keyheight
+        self.font = font
+        self.keycolor = keycolor
+        self.textcolor = textcolor
+        self.x = x
+        self.y = y
+        
+        self._init_keys()
+
+        # resize to fit keys
+        self.update_idletasks()
+        self.geometry('{}x{}+{}+{}'.format(self.winfo_width(),
+                                           self.winfo_height(),
+                                           int(self.winfo_width()/2), int(self.winfo_height() * 3.5))) #+ int(keyheight/4)))
+        
+    def _init_keys(self):
+        self.row1 = Frame(self)
+        self.row2 = Frame(self)
+        self.row3 = Frame(self)
+        self.row4 = Frame(self)
+
+        self.row1.grid(row=1)
+        self.row2.grid(row=2)
+        self.row3.grid(row=3)
+        self.row4.grid(row=4)
+
+        self.alpha = {
+            'row1' : ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '←'],
+            'row2' : ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k'],
+            'row3' : ['PREV','l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't','NEXT'],
+            'row4' : ['↑','u', 'v', 'w', 'x', '␣', 'y', 'z', '.', '@', "❌"]
+            }
+        
+        for row in self.alpha: # iterate over dictionary of rows
+            if row == 'row1':             # TO-DO: re-write this method
+                i = 1                     # for readability and functionality
+                for k in self.alpha[row]:
+                    Button(self.row1,
+                           text=k,
+                           font=self.font,
+                           width=self.keysize,
+                           height=self.keyheight,
+                           bg=self.keycolor,
+                           fg=self.textcolor,
+                           command=lambda k=k: self._attach_key_press(k)).grid(row=0,column=i)
+                    i += 1
+            elif row == 'row2':
+                i = 2
+                for k in self.alpha[row]:
+                    Button(self.row2,
+                           text=k,
+                           font=self.font,
+                           width=self.keysize,
+                           height=self.keyheight,
+                           bg=self.keycolor,
+                           fg=self.textcolor,
+                           command=lambda k=k: self._attach_key_press(k)).grid(row=0,column=i)
+                    i += 1
+            elif row == 'row3':
+                i = 2
+                for k in self.alpha[row]:
+                    Button(self.row3,
+                           text=k,
+                           font=self.font,
+                           width=self.keysize,
+                           height=self.keyheight,
+                           bg=self.keycolor,
+                           fg=self.textcolor,
+                           command=lambda k=k: self._attach_key_press(k)).grid(row=0,column=i)
+                    i += 1
+            else:
+                i = 3
+                for k in self.alpha[row]:
+                    if k == '[ space ]':
+                        Button(self.row4,
+                               text=k,
+                               font=self.font,
+                               width=self.keysize,
+                               height=self.keyheight,
+                               bg=self.keycolor,
+                               fg=self.textcolor,
+                               command=lambda k=k: self._attach_key_press(k)).grid(row=0,column=i)
+                    else:
+                        Button(self.row4,
+                               text=k,
+                               font=self.font,
+                               width=self.keysize,
+                               height=self.keyheight,
+                               bg=self.keycolor,
+                               fg=self.textcolor,
+                               command=lambda k=k: self._attach_key_press(k)).grid(row=0,column=i)
+                    i += 1
+
+    def _init_capital_keys(self):
+        self.row1.destroy()
+        self.row2.destroy()
+        self.row3.destroy()
+        self.row4.destroy()
+
+        self.row1 = Frame(self)
+        self.row2 = Frame(self)
+        self.row3 = Frame(self)
+        self.row4 = Frame(self)
+
+        self.row1.grid(row=1)
+        self.row2.grid(row=2)
+        self.row3.grid(row=3)
+        self.row4.grid(row=4)
+
+
+
+        self.alpha = {
+            'row1' : ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '←'],
+            'row2' : ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K'],
+            'row3' : ['PREV','L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T','NEXT'],
+            'row4' : ['↓', 'U', 'V', 'W', 'X', '␣', 'Y', 'Z', '.', '@', "❌"]
+            }
+
+        for row in self.alpha: # iterate over dictionary of rows
+            if row == 'row1':             # TO-DO: re-write this method
+                i = 1                     # for readability and functionality
+                for k in self.alpha[row]:
+                    Button(self.row1,
+                           text=k,
+                           font=self.font,
+                           width=self.keysize,
+                           height=self.keyheight,
+                           bg=self.keycolor,
+                           fg=self.textcolor,
+                           command=lambda k=k: self._attach_key_press(k)).grid(row=0,column=i)
+                    i += 1
+            elif row == 'row2':
+                i = 2
+                for k in self.alpha[row]:
+                    Button(self.row2,
+                           text=k,
+                           font=self.font,
+                           width=self.keysize,
+                           height=self.keyheight,
+                           bg=self.keycolor,
+                           fg=self.textcolor,
+                           command=lambda k=k: self._attach_key_press(k)).grid(row=0,column=i)
+                    i += 1
+            elif row == 'row3':
+                i = 2
+                for k in self.alpha[row]:
+                    Button(self.row3,
+                           text=k,
+                           font=self.font,
+                           width=self.keysize,
+                           height=self.keyheight,
+                           bg=self.keycolor,
+                           fg=self.textcolor,
+                           command=lambda k=k: self._attach_key_press(k)).grid(row=0,column=i)
+                    i += 1
+            else:
+                i = 3
+                for k in self.alpha[row]:
+                    if k == '[ space ]':
+                        Button(self.row4,
+                               text=k,
+                               font=self.font,
+                               width=self.keysize,
+                               height=self.keyheight,
+                               bg=self.keycolor,
+                               fg=self.textcolor,
+                               command=lambda k=k: self._attach_key_press(k)).grid(row=0,column=i)
+                    else:
+                        Button(self.row4,
+                               text=k,
+                               font=self.font,
+                               width=self.keysize,
+                               height=self.keyheight,
+                               bg=self.keycolor,
+                               fg=self.textcolor,
+                               command=lambda k=k: self._attach_key_press(k)).grid(row=0,column=i)
+                    i += 1
+
+    def _destroy_popup(self):
+        self.destroy()
+
+    def _attach_key_press(self, k):
+        if k == 'NEXT':
+            self.attach.tk_focusNext().focus_set()
+            self.destroy()
+        elif k == 'PREV':
+            self.attach.tk_focusPrev().focus_set()
+            self.destroy()
+        elif k == '←':
+            self.attach.delete(len(self.attach.get())-1, END)
+        elif k == '␣':
+            self.attach.insert(END, ' ')
+        elif k == '❌':
+            self.destroy()
+        elif k == '↑':
+            self._init_capital_keys()
+        elif k == '↓':
+            self.row1.destroy()
+            self.row2.destroy()
+            self.row3.destroy()
+            self.row4.destroy()
+            self._init_keys()
+        else:
+            self.attach.insert(END, k)
+
+'''
+TO-DO: Implement Number Pad
+class _PopupNumpad(Toplevel):
+    def __init__(self, x, y, keycolor='gray', keysize=5):
+        Toplevel.__init__(self, takefocus=0)
+        
+        self.overrideredirect(True)
+        self.attributes('-alpha',0.85)
+
+        self.numframe = Frame(self)
+        self.numframe.grid(row=1, column=1)
+
+        self.__init_nums()
+
+        self.update_idletasks()
+        self.geometry('{}x{}+{}+{}'.format(self.winfo_width(),
+                                           self.winfo_height(),
+                                           self.x,self.y))
+
+    def __init_nums(self):
+        i=0
+        for num in ['7','4','1','8','5','2','9','6','3']:
+            print num
+            Button(self.numframe,
+                   text=num,
+                   width=int(self.keysize/2),
+                   bg=self.keycolor,
+                   command=lambda num=num: self.__attach_key_press(num)).grid(row=i%3, column=i/3)
+            i+=1
+'''
+
+class KeyboardEntry(Frame):
+    '''An extension/subclass of the Tkinter Entry widget, capable
+    of accepting all existing args, plus a keysize and keycolor option.
+    Will pop up an instance of _PopupKeyboard when focus moves into
+    the widget
+
+    Usage:
+    KeyboardEntry(parent, keysize=6, keycolor='white').pack()
+    '''
+    
+    def __init__(self, parent, keysize=5, keycolor="white", textcolor="#1b709d", *args, **kwargs):
+        Frame.__init__(self, parent)
+        self.parent = parent
+        
+        self.entry = Entry(self, *args, **kwargs)
+        self.entry.pack()
+
+        self.keysize = keysize
+        self.keycolor = keycolor
+        self.textcolor = textcolor
+        
+        self.state = 'idle'
+        
+        self.entry.bind('<FocusIn>', lambda e: self._check_state('focusin'))
+        self.entry.bind('<FocusOut>', lambda e: self._check_state('focusout'))
+        self.entry.bind('<Key>', lambda e: self._check_state('keypress'))
+        _PopupKeyboard(attach=self.entry,
+                                 parent=self.parent,
+                                 x=self.entry.winfo_rootx(),
+                                 y=self.entry.winfo_rooty() + self.entry.winfo_reqheight(),
+                                 keysize=self.keysize,
+                                 textcolor=self.textcolor,
+                                 keycolor=self.keycolor)
+
+
+def test():  
+    root = Tk()
+    KeyboardEntry(root, keysize=6, keycolor='white').pack()
+    KeyboardEntry(root).pack()
+    root.mainloop()
+
+if __name__ == '__main__':
+    test()
